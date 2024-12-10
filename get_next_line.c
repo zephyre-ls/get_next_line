8a23b3af6eb6db0f976c72d72b061c59aff1fd78
Zephyre <laurelineduf@proton.me>
2024-12-10 19:17:14 +0100

    juste les fichiers

---
+++ get_next_line.c
@@ -0,0 +1,26 @@
+#include "get_next_line"
+
+Fonction ok = MALLOC, FREE, READ
+Return = Contenue de la ligne lue, NULL si rien d autre a lire ou error
+Descriptor = Ecrire unefonction qui retourne une ligne lue depuis un descripteur de fichier
+GNL_utils = fonction supplementaires necessaire a la realisation de GNL
+Compilation = cc -Wall -Wextra -Werror -D BUFFER_SIZE=42 <files>.c
+
+INTERDIT = libft, ft lseek, variable globales
+
+VARIABLE STATIQUE !
+
+char	*get_next_line(int fd)
+{
+
+
+}
+
+int	main(void)
+{
+
+	return (0);
+}
+
+
+/=∂/\/\[](_)§ /\/\@|V †|-|@¯|¯ /-/!570®1<|-\£1_`/ ¢@/\/\ε vv!7}{ ??? = leet speak (utilisation caractere alphanumerique ASCII) 
---
+++ get_next_line.h
@@ -0,0 +1,9 @@
+#ifndef GET_NEXT_LINE
+# defini GET_NEXT_LINE
+# include <stlib.h>
+# include <unistd.h>
+
+
+char	*get_next_line(int fd);
+
+#endif
