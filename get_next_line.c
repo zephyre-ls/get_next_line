f9b707e1336002607146f5d8d5169f86f36837b0
Zephyre <laurelineduf@proton.me>
2025-01-17 11:38:13 +0100

    modification

--- get_next_line.c
+++ get_next_line.c
@@ -6,51 +6,9 @@
 /*   By: lduflot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 11:13:25 by lduflot           #+#    #+#             */
-/*   Updated: 2025/01/17 11:27:28 by lduflot          ###   ########.fr       */
+/*   Updated: 2025/01/17 11:35:52 by lduflot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*Fonction ok = MALLOC, FREE, READ
-Return = Contenue de la ligne lue, NULL si rien d autre a lire ou error
-Descriptor = Ecrire unefonction qui retourne une ligne lue 
-depuis un descripteur de fichier
-GNL_utils = fonction supplementaires necessaire a la realisation de GNL
-Compilation = cc -Wall -Wextra -Werror -D BUFFER_SIZE=42 <files>.c
-Buffer = (tampon en francais) est une zone de memoire utilise 
-pour sotcker temporairement des donnes pendant qu elle sont 
-transferer entredeux endroits 
-INTERDIT = libft, ft lseek, variable globales
-VARIABLE STATIQUE = variable qui conserve sa valeur entre plusieurs appels 
-de fonctions c a d qu elle ne perd pas sa valeur quand la fonction se termine 
-
-ssize_t read(int fd, void *buf, size_t count);
-
-int fd= 0 entrée standard (stdin, clavier)
-				1 sortie standard (stdout l ecran)
-				2 erreur standar (stderr)
-
-open action selon type d'ouverture =
-O_RDONLY = lecture seule
-O_WRONLY = ecriture seule
-O_RDWR = both 
-
-void *buf = pointeur vers une zone de memoire ou les donnes lu seronts stocke 
-size_t count= nombre max d octet a lire 
-lire dans un fichier et stocke dans une variable, a une static donc 
-si je redemande ( byte va prendre apres les 5 premiers byte demander 
-la premiere fois. (Donc comporte une static)
-
-Difference entre size_t et ssize_t = 
-	_ size_t = type non signé (unsigned) = que des valeurs positives 
-		ssize_t = type signé les deux valeurs,
-		dans le cas de read utilisations de ssize-t puise renvoie erreur 
-		si jamais n a pas reussis a lire le fichier 
-exemple file = Hello$
-							good bye
-buff = après appel = Hello
-byte 5
-return = 5
-*/
 
 #include "get_next_line.h"
 
